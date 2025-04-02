3c793417fa9e83806aab7d4409931da06351d249
Sabrina-Tseng <tsens543@newschool.edu>
2025-04-02 19:30:03 -0400

    upload to git

---
+++ js/scripts.js
@@ -0,0 +1,111 @@
+const search =  document.getElementById('searchForm');
+
+search.addEventListener('submit', function(e){
+   
+	e.preventDefault();
+
+	const platenum = search.elements.platenum;
+
+	// const cellrow = search.elements.cellrow;
+	// const cellcolumn = search.elements.cellcolumn;
+	// newCell(parseInt(platenum.value), alphabetToNumber(cellrow.value), parseInt(cellcolumn.value))
+
+
+	const cell = search.elements.cell;
+	const cellsplit = cell.value.split("");
+	// console.log(cellsplit);
+
+	newCell(parseInt(platenum.value), alphabetToNumber(cellsplit[0]), parseInt(cellsplit[1]+cellsplit[2]+cellsplit[3]))
+})
+
+
+function newCell(platenum, cellrow, cellcolumn) {
+
+	// console.log(" ");
+	// console.log("platenum:" + platenum);
+	// console.log("cellrow:" + cellrow);
+	// console.log("cellcolumn:" + cellcolumn);
+
+	//===== check if in range =====
+
+	if (cellcolumn < 1 || cellcolumn > 24 || cellrow < 1 || cellrow > 16 || platenum < 1){
+		showAns("error: input out of range");
+		return;
+	}
+
+	//===== caculate plate number =====
+
+	let newplatenum = 1;
+	let sectioninbigplate = 1;
+
+	if (cellcolumn <= 12 && cellrow <=8) {
+		newplatenum = platenum*4 -3;
+		sectioninbigplate = 1;
+	} 
+	else if (cellcolumn >=13 && cellrow <=8){
+		newplatenum = platenum*4 -2;
+		sectioninbigplate = 2;
+	}
+	else if (cellcolumn <=12 && cellrow >=9){
+		newplatenum = platenum*4 -1;
+		sectioninbigplate = 3;
+	}
+	else if (cellcolumn >=13 && cellrow >=9){
+		newplatenum = platenum*4;
+		sectioninbigplate = 4;
+	}
+	else{
+		showAns("pleate number error");
+		return;
+	}
+
+	//===== convert to small plate =====
+
+	//on the right sections, row number -12
+	if (sectioninbigplate == 2 || sectioninbigplate == 4){
+		cellcolumn -= 12;
+	}
+	//on the bottom sections, column number -8
+	if (sectioninbigplate == 3 || sectioninbigplate == 4){
+		cellrow -= 8;
+	}
+
+	//===== column adjust =====
+
+	if (cellcolumn <=5) {
+		cellcolumn += 1;
+	} 
+	else if (cellcolumn >=8) {
+		cellcolumn -= 1;
+	}else{
+		showAns("error: this is a controll column");
+		return;
+	}
+
+	showAns("Plate:" + newplatenum + "<br>Cell:" + numberToAlphabet(cellrow)+ cellcolumn);
+
+}
+
+function showAns(ans){
+	const answer =  document.getElementById('answer');
+	while (answer.firstChild) {
+		answer.removeChild(answer.firstChild);
+	  }
+	answer.insertAdjacentHTML('beforeend', ans)
+}
+
+function alphabetToNumber(char) {
+	const charLower = char.toLowerCase();
+	const charCode = charLower.charCodeAt(0);
+  
+	if (charCode >= 97 && charCode <= 122) {
+	  return charCode - 96; // 'a' is 97, so subtract 96 to get 1-based index
+	} else {
+	  return undefined; // Handle cases where the input is not a letter
+	}
+  }
+
+function numberToAlphabet(number, isUpperCase = true) {
+const start = isUpperCase ? 65 : 97;
+return String.fromCharCode(start + number - 1);
+}
